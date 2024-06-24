fb999b06fb13e9b1a27b533365bc6acee85a5067
ShutUp-Pls <marcodelgado.1008@gmail.com>
2024-06-24 18:06:01 -0400

    Commit Inicial
    Se desarrolló el Item 1 y el Item 2 en su totalidad (Reticente a cambios)

---
+++ Item1_Algoritmo.py
@@ -0,0 +1,39 @@
+import numpy as np
+
+def diferencias_finita(l, t, a, m=2, n=2, f=lambda:None, g=lambda:None):
+    # Paso 1
+    h = l/m
+    k = t/n
+    y = (a*k)/h
+
+    w = np.zeros((m+1, n+1)) # Matriz W PlaceHolder
+    # Paso 2
+    for j in range(1, n+1):
+        w[0, j] = 0
+        w[m, j] = 0
+
+    # Paso 3
+    w[0, 0] = f(0)
+    w[m, 0] = f(l)
+
+    # Paso 4
+    for i in range(1, m):
+        w[i, 0] = f(i*h)
+        w[i, 1] = (1-(y**2))*f(i*h) + ((y**2)/2)*(f((i+1)*h)+f((i-1)*h)) + k*g(i*h)
+
+    # Paso 5
+    for j in range(1, n):
+        for i in range(1, m):
+            w[i, j+1] = 2*(1-(y**2))*w[i, j] + (y**2)*(w[i+1, j] + w[i-1, j]) - w[i, j-1]
+
+    resultados = [] # Salida (x, t, w)
+    # Paso 6
+    for j in range(n+1):
+        t = j*k
+        for i in range(m+1):
+            x = i*h
+            resultados.append((x, t, w[i, j]))
+
+    # Paso 7
+    return resultados
+
---
+++ Item2_Aplicacion.py
@@ -0,0 +1,76 @@
+import csv # Para exportar los resultados a un archivo CSV para facilitar trabajo con geogebra
+import numpy as np
+
+from Item1_Algoritmo import diferencias_finita
+
+# DEFINICIÓN DE CONDICIONES INICIALES
+'''
+- Condición inicial para el metodo:
+u(x,0) = f(x)
+
+- En el problema se define:
+u(x,t) = sin(pi*x)*cos(2*pi*t)
+
+- De lo anterior obtenemos que la condición inicial f(x) es:
+u(x,0) = f(x) = sin(pi*x)
+'''
+
+def f(x): return np.sin((np.pi)*x)
+
+'''
+# Definición de las funciones f y g para el problema #
+
+- Condición inicial para el metodo:
+∂u/∂t(x,0) = g(x)
+
+- En el problema se define:
+∂u/∂t(x,0) = 0
+
+- De lo anterior obtenemos que la condición inicial g(x) es:
+∂u/∂t(x,0) = g(x) = 0
+'''
+
+def g(x): return 0
+
+'''
+- El metodo aproxima solución bajo los intervalos:
+0 < x < L | 0 < t < T
+
+- Los intervalos que define el problema son:
+0 < x < 1 | 0 < t < 1
+
+- Por lo tanto: L = 1 | T = 1
+'''
+
+L = 1
+T = 1
+
+'''
+- La ecuación de onda que aproxima el metodo es:
+(∂²u/∂t²(x,t)) - (alpha²)*(∂²u/∂x²(x,t)) = 0
+
+- La ecuación de onda dada por el problema es:
+(∂²u/∂t²) - (4)*(∂²u/∂x²) = 0
+
+- De lo anterior podemos obtener el valor de 'alpha':
+alpha² = 4 | alpha = ±2
+'''
+
+alpha = 2
+
+# Parametros dados por la actividad 3 (Variar para )
+M = 10
+N = 10
+
+# Cálculo de las aproximaciones
+resultados = diferencias_finita(L, T, alpha, M, N, f, g)
+
+# Guardar para exportar los resultados como .csv
+# Usar .csv facilita su incersión a softwares de graficos
+# GeoGebra en su versión classic permite la inserción de tablas
+if True:
+    with open('resultados.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(["x", "y", "z"])
+        for res in resultados:
+            writer.writerow([res[0], res[1], res[2]])
